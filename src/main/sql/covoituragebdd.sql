64b37b3bfebd469568c0d64d25995c000ce3013a
Michael <michael.rupp@hei.fr>
2014-05-02 14:37:25 +0200

    bdd

--- src/main/sql/covoituragebdd.sql
+++ src/main/sql/covoituragebdd.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client: localhost
--- Généré le: Lun 24 Février 2014 à 15:18
+-- Généré le: Ven 02 Mai 2014 à 12:36
 -- Version du serveur: 5.6.12-log
 -- Version de PHP: 5.4.12
 
@@ -33,13 +33,29 @@ CREATE TABLE IF NOT EXISTS `annonceproposition` (
   `estReponseARecherche` tinyint(1) NOT NULL,
   `villeDepart` varchar(50) NOT NULL,
   `villeArrivee` varchar(50) NOT NULL,
-  `dateEtHeureTrajet` datetime NOT NULL,
+  `dateEtHeureTrajet` varchar(25) NOT NULL,
   `commentaire` text NOT NULL,
   `prix` int(11) NOT NULL,
   `nbPlace` int(11) NOT NULL,
   `login` varchar(6) NOT NULL,
   PRIMARY KEY (`idAnnonceProposition`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=11 ;
+
+--
+-- Contenu de la table `annonceproposition`
+--
+
+INSERT INTO `annonceproposition` (`idAnnonceProposition`, `estReponseARecherche`, `villeDepart`, `villeArrivee`, `dateEtHeureTrajet`, `commentaire`, `prix`, `nbPlace`, `login`) VALUES
+(1, 0, 'Bethune', 'Lille', '02/05/20141030', 'Bonjour', 5, 4, 'h11362'),
+(2, 0, 'marseille', 'marseille', '02/05/20140711', 'sdfds', 58, 2, 'h11362'),
+(3, 0, 'ville', 'arrive', '16/05/20140000', 'xcvxc', 20, 5, 'h11362'),
+(4, 0, 'ville', 'arrive', '16/05/20140000', 'xcvxc', 20, 5, 'h11362'),
+(5, 0, 'ville', 'arrive', '16/05/20140000', 'xcvxc', 20, 5, 'h11362'),
+(6, 0, 'bethune', 'Lille', '02/05/20141000', 'comment', 5, 2, 'h11362'),
+(7, 0, 'bethune', 'Lille', '02/05/20141001', 'comment', 5, 2, 'h11362'),
+(8, 0, 'Lille', 'Lille', '16/05/20140205', 'cvb', 2, 5, 'h11362'),
+(9, 0, 'Marseille', 'Marseille', '31/05/20140307', 'fddsf', 25, 5, 'h11362'),
+(10, 0, 'Marseille', 'Marseille', '31/05/20140307', 'fddsf', 25, 5, 'h11362');
 
 -- --------------------------------------------------------
 
@@ -51,11 +67,19 @@ CREATE TABLE IF NOT EXISTS `annoncerecherche` (
   `idAnnonceRecherche` int(11) NOT NULL AUTO_INCREMENT,
   `villeDepartRecherche` varchar(50) NOT NULL,
   `villeArriveeRecherche` varchar(50) NOT NULL,
-  `dateEtHeureRecherche` datetime NOT NULL,
+  `dateEtHeureRecherche` varchar(50) NOT NULL,
   `commentaireRecherche` text NOT NULL,
   `login` varchar(6) NOT NULL,
   PRIMARY KEY (`idAnnonceRecherche`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+--
+-- Contenu de la table `annoncerecherche`
+--
+
+INSERT INTO `annoncerecherche` (`idAnnonceRecherche`, `villeDepartRecherche`, `villeArriveeRecherche`, `dateEtHeureRecherche`, `commentaireRecherche`, `login`) VALUES
+(1, '127.0.0.1', 'LaTerre', '24/05/2014119', 'test', 'h11362'),
+(2, 'koukou', 'saymwa', '15/05/2014310', 'allo', 'h11362');
 
 -- --------------------------------------------------------
 
@@ -66,10 +90,20 @@ CREATE TABLE IF NOT EXISTS `annoncerecherche` (
 CREATE TABLE IF NOT EXISTS `etapes` (
   `idEtape` int(11) NOT NULL AUTO_INCREMENT,
   `idAnnonceProposition` int(11) NOT NULL,
-  `nomVille` int(11) NOT NULL,
+  `nomVille` varchar(50) NOT NULL,
   `ordre` int(11) NOT NULL,
   PRIMARY KEY (`idEtape`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+
+--
+-- Contenu de la table `etapes`
+--
+
+INSERT INTO `etapes` (`idEtape`, `idAnnonceProposition`, `nomVille`, `ordre`) VALUES
+(1, 7, 'LaBassee', 0),
+(2, 7, 'Lomme', 1),
+(3, 7, 'Englos', 2),
+(4, 10, 'LaBassee', 0);
 
 -- --------------------------------------------------------
 
@@ -109,11 +143,22 @@ CREATE TABLE IF NOT EXISTS `reserver` (
 CREATE TABLE IF NOT EXISTS `utilisateur` (
   `login` varchar(6) NOT NULL,
   `email` varchar(50) NOT NULL,
+  `password` varchar(32) NOT NULL,
+  `ip` varchar(25) NOT NULL,
   `nom` varchar(50) NOT NULL,
   `prenom` varchar(50) NOT NULL,
   `telephone` varchar(10) NOT NULL,
+  `registered` datetime NOT NULL,
   PRIMARY KEY (`login`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Contenu de la table `utilisateur`
+--
+
+INSERT INTO `utilisateur` (`login`, `email`, `password`, `ip`, `nom`, `prenom`, `telephone`, `registered`) VALUES
+('h11360', 'test@hotmail.fr', 'a7de6a39d7d727ec7b8a33cc87970085', '192.168.0.47', 'RUPP', 'MichaÃ«l', '0698329149', '2014-04-30 01:16:04'),
+('h11362', 'michael.rupp@hei.fr', 'a7de6a39d7d727ec7b8a33cc87970085', '', 'Rupp', 'Michaël', '0698329149', '2014-04-30 00:00:00');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
